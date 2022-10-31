8c0a582394713c9cd3d49f5b8ab432c529e10859
LukyanenkoMisha <misha20009@gmail.com>
2022-10-31 16:19:33 +0200

    Задание 1 - Школьники и Яблоки

---
+++ Homework4/Homework4.py
@@ -0,0 +1,11 @@
+pupil = int(input('Enter the amount of pupils: '))
+
+apples = int(input('Enter the amount of apples '))
+
+received = apples // pupil
+
+left = apples % pupil
+
+print('Each pupil will get', received, 'apples')
+
+print('Apples left', left)

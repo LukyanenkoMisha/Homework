a1274473e12ca8fda1af648ec82a6f65b424b253
LukyanenkoMisha <misha20009@gmail.com>
2022-10-31 16:33:27 +0200

    Задание 2 - Школьники и Парты

---
+++ Homework4/Homework4_1.py
@@ -0,0 +1,5 @@
+class1 = int(input('Enter the amount of students: '))
+class2 = int(input('Enter the amount of students: '))
+class3 = int(input('Enter the amount of students: '))
+
+print ((class1 // 2 + class1 % 2) + (class2 // 2 + class2 % 2) + (class3 // 2 + class3 % 2))

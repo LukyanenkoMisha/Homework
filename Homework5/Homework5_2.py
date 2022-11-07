66a52fb12f332d338dd0033ce81e85c3a2f5ad06
LukyanenkoMisha <misha20009@gmail.com>
2022-11-07 14:29:46 +0200

    Задание 2 Числа в квадрате

--- Homework5/Homework5_2.py
+++ Homework5/Homework5_2.py
@@ -4,8 +4,8 @@ var_2 = var
 while var_2 > 0:
     var_1 += 1
     var_2 //= 10
-for i in range(1, var + 1):
+for j in range(1, var + 1):
     for degree in range(1, var_1 + 1):
-        if i == i * i % 10 ** degree:
-            print(i, '*', i, '=', i * i)
+        if j == j * j % 10 ** degree:
+            print(j, '*', j, '=', j * j)
             break
